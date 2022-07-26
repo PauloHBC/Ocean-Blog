9a36a5f7612e19dcaa0a1c7254518bfaa8f00bf0
Paulo Henrique <paulo.linkinpark.12@gmail.com>
2022-07-26 23:06:04 +0000

    Atualização

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask ("hello")
 
-@app.route("/")
-def hello():
-    return "Hello World"
+posts = [
+    {
+        "title": "O meu primeiro post",
+        "body": "Aqui é o texto do post",
+        "author": "Paulo",
+        "created":datetime(2022,7,25)
+    },
+    {
+        "title": "O meu segundo post",
+        "body": "Texto do segundo post",
+        "author": "Claudio",
+        "created": datetime(2022,7,26)
+    }
 
-@app.route("/meuContato")
-def meuContato():
-    return render_template('index.html')
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
